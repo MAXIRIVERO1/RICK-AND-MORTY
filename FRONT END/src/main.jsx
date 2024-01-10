882210959a5537cfa91b329838ee48c45d2d45d6
MAXIRIVERO1 <maximilianoa.castillorivero@gmail.com>
2024-01-10 00:02:09 -0300

    corrección

--- FRONT END/src/main.jsx
+++ FRONT END/src/main.jsx
@@ -6,23 +6,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from "./Redux/Store/store.js";
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//     <App />
-//     </BrowserRouter>
-//   </React.StrictMode>,
-// )
-
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </Provider>,
-//     document.getElementById('root')
-// );
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
